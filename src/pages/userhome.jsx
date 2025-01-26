98c58f0ef2aab4090cf5d5308c8133a23a2313d4
aayush dev <theaayushdev07@gmail.com>
2025-01-26 19:00:43 +0545

    feat added

--- src/pages/userhome.jsx
+++ src/pages/userhome.jsx
@@ -1,20 +1,47 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+function Page() {
+  return (
+    <div className="page-container">
+      <h1 className="page-title">Welcome to Pregnify </h1>
+      <p className="page-description">
+        Our site will guide you from basic to high level knoweldge about Pregnancy
+  
+        </p>  
+        <p1 className="page-description"><b> Your Health is Our Top Priority</b></p1>  
+    <div className="page-sections">
+    <div className="section-card">
+      <h2 className="page-title">Pregnancy</h2>
+
+      <p>Find tips, Books and Resources and Baby Names</p>
+
+      <a href="#pregnancy" className="section-link">Learn More</a>
+        </div>      
+      </div>     
+    </div>
+  );
+}
+
 const Top = () => {
   return (
-    <nav className="navbar">
-      <ul className="navbar-links">
-  
-      <li><a href="#pregnancy ">Pregnancy </a></li>
-      <li><a href="#baby">Baby</a></li>
-      <li><a href="#parenting">Parenting </a></li>
+    <>
+      
+      <nav className="navbar">
+        <ul className="navbar-links">
+          <li><a href="#pregnancy">Pregnancy</a></li>
+          <li><a href="#baby">Baby</a></li>
+          <li><a href="#parenting">Parenting</a></li>
           <li><Link to="/about">About Us</Link></li>
-      </ul>
-      <div className="navbar-icons">
-        <button className="club-button">Pregnify</button>
-      </div>
-    </nav>
+        </ul>
+        <div className="navbar-icons">
+          <button className="club-button">Pregnify</button>
+        </div>
+      </nav>
+
+      
+      <Page />
+    </>
   );
 };
 
